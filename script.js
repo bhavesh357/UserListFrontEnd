b323cdfa2444e9e142170338e81f52963aae941f
bhavesh357 <bkadam357@gmail.com>
2020-07-12 22:22:36 +0530

    ADDED: js to dynamically add users

---
+++ script.js
@@ -0,0 +1,118 @@
+var users=[
+    {
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kadam",
+        email:"bkadam357@gmail.com",
+        status:"active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhavesh Kale",
+        email:"bkadam357@gmail.com",
+        status:"inactive",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    }]
+    
+    var userList=""
+    for(var i=0;i<users.length;i++){
+        userList+="<div class=\"user-list-item\">\n"+
+        "<div class=\"name list-column\">"+users[i].name+"</div>\n"+
+        "<div class=\"email list-column\">"+users[i].email+"</div>\n";
+        if(users[i].status.match("Active")){
+            userList+="<div class=\"status list-column\"><div class=\"status-active\">"+users[i].status+"</div> </div>\n";
+        }else{
+            userList+="<div class=\"status list-column\"><div class=\"status-inactive\">"+users[i].status+"</div> </div>\n";
+        }
+        userList+="<div class=\"role list-column\">"+users[i].role+"</div>\n"+
+        "<div class=\"last-login list-column\">"+users[i].lastLogin+"</div>\n"+
+        "<div class=\"permission list-column\">"+users[i].permisiion+"</div>\n"+
+        "<button class=\"option list-column\">...</button>\n"+
+        "</div>";
+    }
+    window.document.getElementById("user-list-details").innerHTML+=userList;
