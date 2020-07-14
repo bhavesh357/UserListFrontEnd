84e054048816a64d4f1b0491830e7a74ed0188f6
bhavesh357 <bkadam357@gmail.com>
2020-07-15 00:44:28 +0530

    REFACTORED: so search will work

--- script.js
+++ script.js
@@ -99,27 +99,30 @@ var users=[
         permisiion:"Valid"
     }]
     
-    var userList=""
-    for(var i=0;i<users.length;i++){
-        userList+="<div class=\"user-list-item\">\n"+
-        "<div class=\"name list-column\"><i class=\"flaticon-user\"></i>"+users[i].name+"</div>\n"+
-        "<div class=\"email list-column\">"+users[i].email+"</div>\n";
-        if(users[i].status.match("Active")){
-            userList+="<div class=\"status list-column\"><div class=\"status-active\">"+users[i].status+"</div> </div>\n";
-        }else{
-            userList+="<div class=\"status list-column\"><div class=\"status-inactive\">"+users[i].status+"</div> </div>\n";
+    function loadUsers(users){
+        var userList=""
+        for(var i=0;i<users.length;i++){
+            userList+="<div class=\"user-list-item\">\n"+
+            "<div class=\"name list-column\"><i class=\"flaticon-user\"></i>"+users[i].name+"</div>\n"+
+            "<div class=\"email list-column\">"+users[i].email+"</div>\n";
+            if(users[i].status.match("Active")){
+                userList+="<div class=\"status list-column\"><div class=\"status-active\">"+users[i].status+"</div> </div>\n";
+            }else{
+                userList+="<div class=\"status list-column\"><div class=\"status-inactive\">"+users[i].status+"</div> </div>\n";
+            }
+            userList+="<div class=\"role list-column\">"+users[i].role+"</div>\n"+
+            "<div class=\"last-login list-column\">"+users[i].lastLogin+"</div>\n"+
+            "<div class=\"permission list-column\">"+users[i].permisiion+"</div>\n"+
+            "<button class=\"option list-column\">...</button>\n"+
+            "</div>";
         }
-        userList+="<div class=\"role list-column\">"+users[i].role+"</div>\n"+
-        "<div class=\"last-login list-column\">"+users[i].lastLogin+"</div>\n"+
-        "<div class=\"permission list-column\">"+users[i].permisiion+"</div>\n"+
-        "<button class=\"option list-column\">...</button>\n"+
-        "</div>";
+        window.document.getElementById("user-list-items").innerHTML+=userList;
+        var userCount= window.document.getElementsByClassName("user-list-item").length;
+        window.document.getElementById("user-count").innerHTML = "Users("+userCount+")";
     }
-    window.document.getElementById("user-list-details").innerHTML+=userList;
-    var userCount= window.document.getElementsByClassName("user-list-item").length;
-    window.document.getElementById("user-count").innerHTML = "Users("+userCount+")";
-
-
+    
+    
+    loadUsers(users);
     //code to hide/show sub menu
     var userToggle=true;
     function showSubMenu(value){
@@ -136,8 +139,8 @@ var users=[
             userToggle=true;
         }
     }
-
-
+    
+    
     //code to toggle users
     var userListToggle=true;
     function showUsers(){
@@ -154,3 +157,26 @@ var users=[
             userListToggle=true;
         }
     }
+    
+    
+    //code to search
+    function search(){
+        var word=document.getElementById("user-search-mini").value.toLowerCase();
+        console.log(word);
+        var searchUsers=[];
+        if(word.trim()==""){
+            searchUsers=users;
+        }else{
+            for(var i=0;i<users.length;i++){
+                if(users[i].name.toLowerCase().indexOf(word)>-1 || users[i].email.toLowerCase().indexOf(word)>-1 || users[i].status.toLowerCase().indexOf(word)==0 || users[i].role.toLowerCase().indexOf(word)>-1 ){
+                    searchUsers.push(users[i]);
+                }
+            }
+        }
+        console.log(searchUsers);
+        window.document.getElementById("user-list-items").innerHTML="";
+        loadUsers(searchUsers);
+        
+    }
+    
+    document.getElementById("user-search-mini").addEventListener(oninput,search);
